2819d9792755723fa5ea8f2fac079a1fb5c712d5
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-26 17:41:57 -0700

    Add imports, variables and Start/Stop/Reset/Running methods

--- MyStopwatchapp/app/src/main/java/org/jlmarks/mystopwatchapp/StopwatchActivity.java
+++ MyStopwatchapp/app/src/main/java/org/jlmarks/mystopwatchapp/StopwatchActivity.java
@@ -1,13 +1,57 @@
 package org.jlmarks.mystopwatchapp;
 
+import android.os.Handler;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    //Methods to start/stop/reset the stopwatch
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    //Method to run the timer by looping through the code
+    //once per second.
+    //Thankfully I've read ahead and know that we change parts of it.
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        })
     }
 }
