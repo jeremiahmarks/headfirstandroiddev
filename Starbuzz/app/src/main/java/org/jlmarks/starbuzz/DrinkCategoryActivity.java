85b1ac70fbcc5ba250cf8c33ab3c67c220d9434e
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-27 23:05:37 -0700

    second test drive - added drink category activity

---
+++ Starbuzz/app/src/main/java/org/jlmarks/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,22 @@
+package org.jlmarks.starbuzz;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends ListActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
+
+    }
+
+}
--- Starbuzz/app/src/main/java/org/jlmarks/starbuzz/TopLevelActivity.java
+++ Starbuzz/app/src/main/java/org/jlmarks/starbuzz/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package org.jlmarks.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView,
+                                    View v,
+                                    int position,
+                                    long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
