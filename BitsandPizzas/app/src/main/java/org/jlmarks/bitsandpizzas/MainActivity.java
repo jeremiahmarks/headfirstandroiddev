aad490a0bfbaebc2fa7dc343862f6c8cb1744f73
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-29 22:37:41 -0700

    Create menu and pass intent to create order activity through menu

--- BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/MainActivity.java
@@ -1,7 +1,10 @@
 package org.jlmarks.bitsandpizzas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //This adds items to the action bar.
+        //The inflater basically inflates an XML wireframe with
+        //an object
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                //TODO write this
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                //TODO write this too
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
