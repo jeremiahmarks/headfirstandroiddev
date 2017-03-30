771bd89a25cf4472211af29c1718f9fa6df177bc
Jeremiah Marks <jeremiah@jlmarks.org>
2017-03-29 22:58:54 -0700

    EndChapter9 Menu and Action bars enabled.

--- BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,17 @@ public class MainActivity extends Activity {
         //The inflater basically inflates an XML wireframe with
         //an object
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is something");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
--- BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/OrderActivity.java
+++ BitsandPizzas/app/src/main/java/org/jlmarks/bitsandpizzas/OrderActivity.java
@@ -1,6 +1,8 @@
 package org.jlmarks.bitsandpizzas;
 
+import android.app.ActionBar;
 import android.app.Activity;
+
 import android.os.Bundle;
 
 public class OrderActivity extends Activity {
@@ -9,5 +11,7 @@ public class OrderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
